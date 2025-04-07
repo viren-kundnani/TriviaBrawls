8ede1a01916ecf1776129bc2fd64b048889bcc49
viren-kundnani <virenkundnani@gmail.com>
2025-04-07 18:21:27 +0530

    Hiding env vars

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ server.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
 
-const uri = 'mongodb+srv://virenkundnani:m6RR80U1pDg5cbuC@cluster0.aaqagx0.mongodb.net/quizGame?retryWrites=true&w=majority&appName=Cluster0';
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, {serverSelectionTimeoutMS: 5000, socketTimeoutMS: 45000 })
 .then(() => console.log('MongoDB Atlas connection successful'))
 .catch(err => console.error('MongoDB Atlas connection error:', err));
